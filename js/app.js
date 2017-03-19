eb956ab6f1683a62311732068b812fd2f5dad4e4
dadreway <andrewshylton@gmail.com>
2017-03-19 16:14:45 -0500

    Final version

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
-var app = angular.module("myShoppingList",[]);
-app.controller("myCtrl", funtion(scope){
-	$scope.products = ["milk","chesse","bread"];
-	$scope.addItem = function (){
-		$scope.errortext = "";
-		if (!$scope.addMe){return;}
-		if ($scope.products.indexOf($scope.addMe)== -1){
-		$scope.products.push($scope.addMe);
-		}else{
-			$scope.errortext = "The item is already on your Shopping List.";
-		}
+'use strict';
 
-	}
-	$scope.removeItem = funtion(x){
-		$scope.products.splice(x,1);
-	}
+var app = angular.module('myShoppingList', []);
+
+app.controller('myCtrl', function($scope) {
+    $scope.products = ["Milk", "Bread", "Cheese"];
+    
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    }
+    
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";
+        $scope.products.splice(x, 1);
+    }  
 });
