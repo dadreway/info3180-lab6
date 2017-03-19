bc8009f821ef176a8b87dca7b5163b45dceb41ab
dadreway <andrewshylton@gmail.com>
2017-03-19 15:41:08 -0500

    Step 4 - Error Handling

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ var app = angular.module("myShoppingList",[]);
 app.controller("myCtrl", funtion(scope){
 	$scope.products = ["milk","chesse","bread"];
 	$scope.addItem = function (){
+		$scope.errortext = "";
+		if (!$scope.addMe){return;}
+		if ($scope.products.indexOf($scope.addMe)== -1){
 		$scope.products.push($scope.addMe);
+		}else{
+			$scope.errortext = "The item is already on your Shopping List.";
+		}
+
+	}
+	$scope.removeItem = funtion(x){
+		$scope.products.splice(x,1);
 	}
 });
