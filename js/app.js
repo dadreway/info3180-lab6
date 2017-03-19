026844d218410029ea27d47804c2b590da6f7d79
dadreway <andrewshylton@gmail.com>
2017-03-19 15:13:53 -0500

    created index.html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 var app = angular.module("myShoppingList",[]);
 app.controller("myCtrl", funtion(scope){
 	$scope.products = ["milk","chesse","bread"];
+	$scope.addItem = function (){
+		$scope.products.push($scope.addMe);
+	}
 });
